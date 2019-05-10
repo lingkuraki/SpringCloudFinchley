865452ef71aa6df72a130645a389e3abca0a4903
kuraki <583378006@qq.com>
2019-05-10 18:07:52 +0800

    study for eureka and robbin

--- microservice-siimple-provider-user/src/main/java/com/kuraki/ProviderUserApplication.java
+++ microservice-siimple-provider-user/src/main/java/com/kuraki/ProviderUserApplication.java
@@ -5,12 +5,14 @@ import com.kuraki.entity.User;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.math.BigDecimal;
 import java.util.stream.Stream;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProviderUserApplication {
 
     public static void main(String[] args) {
