1e7a1aab9b2cd7f13f5f1afd0b01a8fd903d4968
kuraki <583378006@qq.com>
2019-05-10 09:17:46 +0800

    the first commit

---
+++ microservice-siimple-provider-user/src/main/java/com/kuraki/dao/UserRepository.java
@@ -0,0 +1,9 @@
+package com.kuraki.dao;
+
+import com.kuraki.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
